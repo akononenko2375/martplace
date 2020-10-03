9b43efa06276f30af6615f83703b5f689be5f705
Aleksandr Kononenko <sawa2375@gmail.com>
2020-10-03 14:51:00 +0300

    added first page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,40 @@
-$(function(){
+$(function () {
+
+	$('.featured__slider').slick({
+
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrows-left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrows-right.svg"></button>'
+	});
+
+	$('.followers__slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 2,
+		variableWidth: true,
+
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrows-left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrows-right.svg"></button>'
+	});
+
+	$('.feedback__slider').slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		variableWidth: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrows-l.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrows-r.svg"></button>'
+
+	});
+
+	$(".rate-star").rateYo({
+		rating: 4.5,
+		starWidth: "15px",
+		spacing: "3px",
+		readOnly: true
+	});
 
 
-	
+
+
+
+	var mixer = mixitup('.newest__inner-box');
+
 });
